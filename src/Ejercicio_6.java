ed4434e9b529d6f15d974bf68e3587a73aab0191
Arturog70 <98788236+Arturog70@users.noreply.github.com>
2022-09-05 13:19:54 -0500

    En este programa, se multiplicarán los primeros         10 números impares, usando un ciclo for.         Los valores son 1, 3, 5, 7, 9 11,13,15,17 y 19.

---
+++ src/Ejercicio_6.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class Ejercicio_6 {
+    public class Coercion_6 {
+        /*
+        En este programa, se multiplicarán los primeros
+        10 números impares, usando un ciclo for.
+        Los valores son 1, 3, 5, 7, 9 11,13,15,17 y 19.
+         */
+        public static void main ( String [] args){
+            long productosImpares= 1;
+            for( int i = 1; i < 20; i+=2){
+                productosImpares= productosImpares*i;
+            }
+            System.out.println("el producto de multipicar los primeros " +
+                    "10 numeros impares son" + " " +productosImpares);
+        }
+    }
+
+}
