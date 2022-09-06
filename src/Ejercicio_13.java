fb8f703b65fa196a527d6dd6e93a5ae9a24b28f7
Arturog70 <98788236+Arturog70@users.noreply.github.com>
2022-09-06 05:26:18 -0500

    Guarada 10 numeros en un arreglo, y al final, mostramos en pantalla el siguinete orden: primero el primero, el untimo, el segundo, el penultimo...

---
+++ src/Ejercicio_13.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Ejercicio_13 {
+    /* Guarada 10 numeros en un arreglo, y al final, mostramos en pantalla
+    el siguinete orden: primero el primero, el untimo, el segundo, el penultimo...
+     */
+    public static void main ( String [] args){
+        int j = 0;
+        int arr []= new int[10];
+        
+        Scanner sc= new Scanner(System.in);
+        for ( j = 0; j< arr.length; j++)
+        {
+            System.out.println("introduce un numero" + (j+1));
+            arr[j]=sc.nextInt();
+        }
+        System.out.println("Imprime los numeros");
+
+        for( j= 0; j< 10; j++)
+        {
+            System.out.println(arr[j]);
+            System.out.println(arr[9-j]);
+        }
+
+    }
+}
